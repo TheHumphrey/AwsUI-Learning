d65927770970caf834962aa7b59b432ca9472e07
Fernando Barros <fernandob.delima@outlook.com>
2021-01-22 12:54:42 -0300

    clean project with awsui/components-react

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react";
+import ReactDOM from "react-dom";
+
+const App = (): ReactElement => (
+  <div>
+    <h1>Teste</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
